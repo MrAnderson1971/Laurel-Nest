97ff26b4b5317e9307493fc7f6028186b3cd103b
MrAnderson1971 <mrthomasanderson37@gmail.com>
2024-10-10 15:35:22 -0700

    can move player left and right

--- src/world_system.cpp
+++ src/world_system.cpp
@@ -11,7 +11,11 @@ WorldSystem::~WorldSystem() {
 void WorldSystem::init() {
 	m_player = std::make_unique<Entity>();
 	m_player->addComponent(Player());
-	m_player->addComponent(Motion());
+
+	Motion playerMotion;
+	playerMotion.position = glm::vec2(renderSystem.getWindowWidth() / 2.0f, renderSystem.getWindowHeight() / 2.0f);
+	playerMotion.velocity = glm::vec2(0, 0);
+	m_player->addComponent(std::move(playerMotion));
 
 	int playerWidth, playerHeight;
 	GLuint playerTextureID = renderSystem.loadTexture("walk_1.png", playerWidth, playerHeight);
@@ -22,8 +26,8 @@ void WorldSystem::init() {
 
 	Sprite playerSprite;
 	playerSprite.textureID = playerTextureID;
-	playerSprite.height = 1.0f;
-	playerSprite.width = 1.0f;
+	playerSprite.height = 0.1f;
+	playerSprite.width = 0.1f;
 
 	m_player->addComponent(std::move(playerTransform));
 	m_player->addComponent(std::move(playerSprite));
@@ -32,7 +36,11 @@ void WorldSystem::init() {
 }
 
 void WorldSystem::update(float deltaTime) {
-	(void)deltaTime;
+	TransformComponent& t = m_player->getComponent<TransformComponent>();
+	Motion& m = m_player->getComponent<Motion>();
+	m.position += m.velocity;
+	t.position[0] = m.position[0];
+	t.position[1] = m.position[1];
 }
 
 void WorldSystem::render() {
@@ -51,10 +59,12 @@ void WorldSystem::render() {
 // not hard coded in case user wants to customize
 void WorldSystem::initKeyBindings() {
 	keyPressActions[GLFW_KEY_ESCAPE] = [this]() { 
+		std::cout << "escape" << std::endl;
 		renderSystem.closeWindow();
 		};
 
 	keyPressActions[GLFW_KEY_A] = [this]() {
+		std::cout << "start left" << std::endl;
 		m_player->getComponent<Motion>().velocity[0] = -player_speed;
 		};
 
@@ -67,6 +77,7 @@ void WorldSystem::initKeyBindings() {
 		};
 
 	keyReleaseActions[GLFW_KEY_A] = [this]() {
+		std::cout << "end left" << std::endl;
 		m_player->getComponent<Motion>().velocity[0] = 0;
 		};
 
