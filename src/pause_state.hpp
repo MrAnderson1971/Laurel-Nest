932d65f2e0d25db14c4ac9a40aaba04f2d86a809
MrAnderson1971 <mrthomasanderson37@gmail.com>
2024-10-11 19:11:36 -0700

    override

--- src/pause_state.hpp
+++ src/pause_state.hpp
@@ -6,15 +6,13 @@ public:
     PauseState(RenderSystem& renderSystem);
     ~PauseState();
 
-    void init();
-    void on_key(int key, int scancode, int action, int mods);
-    void on_mouse_move(const glm::vec2& position);
-    void on_mouse_click(int button, int action, const glm::vec2& position, int mods);
-    void update(float deltaTime);
-    void render();
-    void cleanup();
-    void pause() {}
-    void resume() {};
+    void init() override;
+    void on_key(int key, int scancode, int action, int mods) override;
+    void on_mouse_move(const glm::vec2& position) override;
+    void on_mouse_click(int button, int action, const glm::vec2& position, int mods) override;
+    void update(float deltaTime) override;
+    void render() override;
+    void cleanup() override;
 
 private:
     float timePassed;
