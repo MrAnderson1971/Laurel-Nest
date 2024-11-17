b8f034089d78320272f412df5c4e8769ca3e0329
Anonymous <anonymous@example.com>
2024-11-16 18:41:47 -0800

    implemented all doors to relevant rooms

--- src/world_system.cpp
+++ src/world_system.cpp
@@ -173,9 +173,9 @@ void WorldSystem::init() {
      
     // Initialize the region
     regionManager->init();
-    current_room = regionManager->setRegion(makeRegion<Cesspit>);
-    // testing bmt
-    //current_room = regionManager->setRegion(makeRegion<Birdmantown>);
+    //current_room = regionManager->setRegion(makeRegion<Cesspit>);
+    //testing bmt
+    current_room = regionManager->setRegion(makeRegion<Birdmantown>);
     PhysicsSystem::setRoom(current_room);
 
     // init tutorial (temp)
