eb4cf19c1f53538a57e8e7ffe4a64a79e1e44cd4
Anonymous <anonymous@example.com>
2024-12-01 17:17:39 -0800

    undo testing

--- src/room_strategy.cpp
+++ src/room_strategy.cpp
@@ -1543,7 +1543,7 @@ Entity LNBossRoomStrategy::execute() {
     Entity m_boss_platform = SetBGElem(g_texture_paths->at(TEXTURE_ASSET_ID::GREATBIRD_PLATFORM), 1.f, 1.f, 0.5f, 0.86f, 0.0f);
 
     // greatbird boss
-    //Entity m_greatbird = GreatBossAISystem::init(m_room);
+    Entity m_greatbird = GreatBossAISystem::init(m_room);
     //SetBGElem(g_texture_paths->at(TEXTURE_ASSET_ID::GREATBIRD_IDLE), 1.f, 1.f, 0.5f, 0.65f, 0.0f);
 
     // ceiling
@@ -1560,7 +1560,7 @@ Entity LNBossRoomStrategy::execute() {
     room.insert(m_ceiling);
     room.insert(m_ground);
     room.insert(m_boss_platform);
-    //room.insert(m_greatbird);
+    room.insert(m_greatbird);
     room.setMusic(Mix_LoadMUS(audio_path("greatBird.wav").c_str()));
     registry.rooms.emplace(m_room, std::move(room));
 
@@ -1723,7 +1723,7 @@ Entity RoomStrategy::SetSpikeObstacle(float width, float height, float xPos, flo
     Entity spike = Entity();
     spike = SetPlatform(g_texture_paths->at(TEXTURE_ASSET_ID::SPIKE), width, height, xPos, yPos);
     Damage spikeDamage;
-    spikeDamage.damage_dealt = 10;
+    spikeDamage.damage_dealt = 20;
     registry.damages.emplace(spike, std::move(spikeDamage));
     return spike;
 }
